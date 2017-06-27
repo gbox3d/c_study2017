dc6008535fb5150861360b4baf611c54f6ec531c
gbox3d <gbox3d@gmail.com>
2017-06-28 08:30:39 +0900

    ...

---
+++ test/consoleTest/consoleTest.cpp
@@ -0,0 +1,14 @@
+// consoleTest.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+//'디버깅하지않고 시작' 으로 하면 자동 종료되지않는다.
+//컨트롤 + f5 로 실행시킨다.
+
+int main()
+{
+	printf_s("hello\n");
+    return 0;
+}
+
