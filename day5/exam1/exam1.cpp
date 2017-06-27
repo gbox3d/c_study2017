6931a20d1410dbb995bf9f9d55a37367866528fa
gbox3d <gbox3d@gmail.com>
2017-06-27 11:48:47 +0900

    ...

--- day5/exam1/exam1.cpp
+++ day5/exam1/exam1.cpp
@@ -6,6 +6,17 @@
 
 int main()
 {
+	printf("%d \n", 12); //10진수
+	printf("%d \n", 014); //8진수
+	printf("%d \n", 0xc); //16진수 
+	//printf("%d \n", b1100);
+
+	printf("%d \n",12);
+	printf("%o \n",12);
+	printf("%x \n",12);
+
+	
+
     return 0;
 }
 
