a316b8e8906ac7e06f2bea4f89310290a250a64a
gbox3d <gbox3d@gmail.com>
2017-07-11 14:58:17 +0900

    ...

---
+++ day15/exam3/tilemap.h
@@ -0,0 +1,19 @@
+#pragma once
+struct S_GAMEMAP {
+	//map
+	int m_nWidth, m_nHeight;
+	int m_Map[1024];
+
+	//tile
+	Image *m_ImageObj;
+	int m_TileWidth;
+	int m_TileHeight;
+};
+
+extern void InitMap(S_GAMEMAP *pMap, int width, int height, TCHAR *szImgFile,
+	int nTileWidth, int nTileHeight
+);
+
+extern void SetMapIndex(S_GAMEMAP *pMap, int x, int y, int index);
+extern void DrawTile(S_GAMEMAP *pMap, Graphics *pGrp, int x, int y, int nTileIndex);
+extern void DrawMap(S_GAMEMAP *pMap, Graphics *pGrp, int xOrg, int yOrg);
