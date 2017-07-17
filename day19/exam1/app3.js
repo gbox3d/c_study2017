a34cb335783e764492f8007dca5768259b1c1566
gbox3d <gbox3d@gmail.com>
2017-07-17 11:33:13 +0900

    ...

---
+++ day19/exam1/app3.js
@@ -0,0 +1,12 @@
+﻿const dgram = require("dgram");
+const udp_socket = dgram.createSocket("udp4");
+
+udp_socket.on("message", function (msg, rinfo) {
+    console.log(rinfo.address + ":" + rinfo.port + ":" + msg);
+
+    strMsg = "nice to meet u " + msg;
+
+    udp_socket.send(Buffer(strMsg), 0, strMsg.length, rinfo.port, rinfo.address);
+});
+
+udp_socket.bind(3333);
---
+++ day19/exam1/app4.js
@@ -0,0 +1,11 @@
+﻿const dgram = require("dgram");
+const udp_socket = dgram.createSocket("udp4");
+
+udp_socket.on("message", function (msg, rinfo) {
+    console.log(rinfo.address + ":" + rinfo.port + ":" + msg);
+    process.exit();
+});
+
+
+strMsg = "hello udp"
+udp_socket.send(Buffer(strMsg), 0, strMsg.length, 3333, '192.168.0.7');
